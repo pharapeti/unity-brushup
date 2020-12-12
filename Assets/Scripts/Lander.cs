9b2cd7e28aa901ee27be49bccc6aa4d4ffe37433
Patrice Harapeti <patrice.harapeti@gmail.com>
2020-12-12 15:32:13 +1100

    Initial commit

---
+++ Assets/Scripts/Lander.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lander : MonoBehaviour
+{
+    [Header("Engine Thrust (N)")]
+    public float engineThrust; // in Newtons (N) in positive Y direction
+
+    private Rigidbody rigidbody;
+
+    // Landing Events
+    public delegate void LandedAction();
+    public static event LandedAction OnLanding;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        rigidbody.AddForce(gameObject.GetComponent<Transform>().transform.up * engineThrust);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (OnLanding != null)
+        {
+            OnLanding();
+        }
+    }
+}
---
+++ Assets/Scripts/UIText.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIText : MonoBehaviour
+{
+    // UI Elements
+    public Text uiText;
+    public Text altitudeText;
+    public Text descentRateText;
+
+    public GameObject lander;
+    public Transform lander_transform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        uiText = GetComponent<Text>();
+        lander_transform = lander.GetComponent<Transform>();
+        Lander.OnLanding += handleLanding; // subscribe handleLanding() to the Lander's onLanding event
+    }
+
+    void FixedUpdate()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(lander_transform.position, Vector3.down, out hit))
+        {
+            GameObject hitObject = hit.collider.gameObject;
+            altitudeText.text = "Altitude: " + System.Math.Round(hit.distance, 2);
+        }
+    }
+
+    void handleLanding()
+    {
+        uiText.text = "We have impact!";
+    }
+}
